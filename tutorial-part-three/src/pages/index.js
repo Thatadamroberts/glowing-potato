6c75a5fffbd3e5318bbab64a35a544dd91ff7961
Adam Roberts <strawsnake@gmail.com>
2019-10-28 22:59:30 +0000

    Completed phase 3 of Gatsby Tutorial

---
+++ tutorial-part-three/src/pages/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Layout from "../components/layout"
+export default () => (
+    <Layout>
+        <h1>Hi! I'm building a fake Gatsby site as part of a tutorial! Soy estudiante de espanol</h1>
+        <p>
+        What do I like to do? Me Gusto leer y dormir.
+        </p>
+    </Layout>
+)
